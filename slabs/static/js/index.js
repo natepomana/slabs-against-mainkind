fac300fb9416f3d244c657d72d53fc96c66c86f4
Nate Pomana <nathanpomana@gmail.com>
2019-02-10 13:36:44 -0500

    begin js

--- slabs/static/js/index.js
+++ slabs/static/js/index.js
@@ -1,1 +1,9 @@
-console.log("hello");
+console.log("asdf");
+
+
+const play = (type,name) => {
+  console.log(type,name);
+  // $.post("url",{type:type, name: name}).done((data)=>{
+  //   console.log(data);
+  // });
+}
