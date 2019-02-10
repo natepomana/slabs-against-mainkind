2d701e1f93581717b3e8f66b1da6a4c5e0a0ce8f
Nate Pomana <nathanpomana@gmail.com>
2019-02-10 14:06:20 -0500

    initial post request
    need to do server work on adding users.

--- slabs/init.py
+++ slabs/init.py
@@ -1,4 +1,5 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request
+import json
 app = Flask(__name__)
 
 @app.route("/hello")
@@ -8,6 +9,20 @@ def home():
 @app.route("/game")
 def game():
     return render_template("game.html")
+
+
+@app.route("/join", methods=['post'])
+def join():
+    type = request.form['type']
+    name = request.form['name']
+    if type == "join" && name != "":
+        # user can join a game.
+        return
+    elif type == "watch":
+        # user can watch a game.
+        return
+    return name
+
 
 if __name__ == '__main__':
     app.run()
--- slabs/static/js/index.js
+++ slabs/static/js/index.js
@@ -1,9 +1,8 @@
-console.log("asdf");
-
 
 const play = (type,name) => {
   if ((type === "join" && name !== "") || type === "watch") {
-    $.post("url",{type:type, name: name}).done((data)=>{
+    $.post("/join",{type:type, name: name}).done((data)=>{
+      console.log("here");
       console.log(data);
     });
   }
