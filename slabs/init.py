e39944747af907bcab09a8a4debfb93ee17515b4
Kevin <kxl3544@rit.edu>
2019-02-10 12:47:44 -0500

    Update init.py

--- slabs/init.py
+++ slabs/init.py
@@ -1,11 +1,8 @@
 app = Flask(__name__)
 
 @app.route("/hello")
-<<<<<<< HEAD
-def main():
-    return "Hello"
-=======
 >>>>>>> ad3529310c2fa7dfeea04429beb91895caad0bcb
+     return render_template("index.html")
 
 
 if __name__ == '__main__':
