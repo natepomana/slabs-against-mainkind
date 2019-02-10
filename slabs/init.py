7e8113ef292131a9a40b1698bb6b8f86f620c03b
Nate Pomana <nathanpomana@gmail.com>
2019-02-10 12:42:17 -0500

    updates

--- slabs/init.py
+++ slabs/init.py
@@ -1,10 +1,10 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 @app.route("/hello")
-def main():
-    print("hello")
+def home():
+     return render_template("index.html")
 
 
 if __name__ == '__main__':
-    main()
+    app.run()
