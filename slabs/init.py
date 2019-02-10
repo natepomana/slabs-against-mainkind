0302a13bc50ee78318197bb7257ce719deaca7fc
Kevin <kxl3544@rit.edu>
2019-02-10 13:46:41 -0500

    Began generic game view

--- slabs/init.py
+++ slabs/init.py
@@ -5,6 +5,9 @@ app = Flask(__name__)
 def home():
      return render_template("index.html")
 
+@app.route("/game")
+def game():
+    return render_template("game.html")
 
 if __name__ == '__main__':
     app.run()
